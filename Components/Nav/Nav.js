d50bc1abdc237af2e4fbd546ac3b4dbd2e53f74c
Noble Obioma <nobioma1@gmail.com>
2019-05-22 18:20:13 +0100

    Nav Component: Add link to react app host   - change hamburger button color for visibility, during toggle of sidebar.   (nav.less):  alter z-index, add spacing, margin and add background-color   to navbar on desktop

--- Components/Nav/Nav.js
+++ Components/Nav/Nav.js
@@ -1,9 +1,12 @@
+const appHost = 'https://essentialismapp.netlify.com';
+
 class Nav {
   constructor(element) {
     this.element = element;
     this.navLinks = this.element.querySelectorAll('.nav-link');
     this.navBtn = document.querySelector('.nav-btn');
     this.navBtn.addEventListener('click', () => this.toogleSidebar());
+    this.login = document.querySelector('.login').href = appHost;
     this.isOpen = false;
   }
 
@@ -13,6 +16,7 @@ class Nav {
         width: '100%',
         onComplete: () => {
           this.navBtn.classList.replace('fa-bars', 'fa-times');
+          this.navBtn.style.color = '#fcf9ED';
         }
       });
       this.navLinks.forEach(nav => {
@@ -26,6 +30,7 @@ class Nav {
         delay: 0.48,
         width: '0%',
         onComplete: () => {
+          this.navBtn.style.color = '#2D283D';
           this.navBtn.classList.replace('fa-times', 'fa-bars');
         }
       });
